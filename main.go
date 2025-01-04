9de282b9c1dadf56661bad377b99f481bf0d9fa4
Roberto Paulino <rpaulinodev@gmail.com>
2025-01-04 12:50:34 -0500

    adds exit command

--- main.go
+++ main.go
@@ -6,7 +6,26 @@ import (
   "bufio"
   "os"
 )
+
+type cliCommand struct {
+  name        string
+  description string
+  callback    func() error
+}
+
+
 func main() {
+
+  
+  commands := map[string]cliCommand{
+
+    "exit": {
+      name: "exit",
+      description: "Exit the Pokedex",
+      callback: commandExit,
+    },
+  }
+
   reader := bufio.NewReader(os.Stdin)
   scanner := bufio.NewScanner(reader)
 
@@ -19,7 +38,18 @@ func main() {
 
     cleanText := cleanInput(text)
 
-    fmt.Printf("Your command was: %v \n", cleanText[0])
+    commandInfo, ok := commands[cleanText[0]] 
+
+    if ok {
+      err := commandInfo.callback()
+
+      if err != nil {
+        fmt.Printf("Error: %v \n input: %v \n running command %v\n", err, cleanText[0], commandInfo.callback)
+      }
+      continue
+    }
+
+    fmt.Println("Unknown command")
   }
 }
 
@@ -30,4 +60,12 @@ func cleanInput(text string) []string {
   split := strings.Split(trimmed, " ")
 
   return split
+}
+
+func commandExit() error {
+  fmt.Println("Closing the Pokedex... Goodbye!")
+
+  os.Exit(0)
+
+  return nil
 }
