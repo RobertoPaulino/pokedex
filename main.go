8116552ef7fff8cc662ffb4d43756a09ddc5f6dc
Roberto Paulino <rpaulinodev@gmail.com>
2025-01-04 11:03:58 -0500

    Changes main() logic to be REPL

--- main.go
+++ main.go
@@ -3,9 +3,24 @@ package main
 import (
   "fmt"
   "strings"
+  "bufio"
+  "os"
 )
 func main() {
-  fmt.Println("Hello, World!")
+  reader := bufio.NewReader(os.Stdin)
+  scanner := bufio.NewScanner(reader)
+
+  var text string
+
+  for {
+    fmt.Printf("Pokedex >")
+    scanner.Scan()
+    text = scanner.Text()
+
+    cleanText := cleanInput(text)
+
+    fmt.Printf("Your command was: %v \n", cleanText[0])
+  }
 }
 
 func cleanInput(text string) []string {
