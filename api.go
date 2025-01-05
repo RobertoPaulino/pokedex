6b95380b3aabce1fb1d2b21f16d1f05b83cfb658
Roberto Paulino <rpaulinodev@gmail.com>
2025-01-04 22:33:39 -0500

    starts implementing unmarshalling of pokeAPI location endpoint

--- api.go
+++ api.go
@@ -1,27 +1,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type area struct {
+  Name string `json:"name"`
+  Url string `json:"url"`
+}
+  
+type locationData struct {
+  Count int `json:"count"`
+  Next string `json:"next"`
+  Previous string `json:"previous"`
+  Results []map[int]area `json:"results"`
+}
 
-func getLocation() (string, error) {
+func getLocation() ([]string, error) {
   res, err := http.Get("https://pokeapi.co/api/v2/location-area/")
   if err != nil {
-    return "", fmt.Errorf("error requesting: %w", err)
+    return []string{}, fmt.Errorf("error requesting: %w", err)
   }
   body, err := io.ReadAll(res.Body)
   defer res.Body.Close()
 
   if res.StatusCode > 299 {
-    return "", fmt.Errorf("Unsucessful response code: %v", res.StatusCode)
+    return []string{}, fmt.Errorf("Unsucessful response code: %v", res.StatusCode)
   }
 
   if err != nil {
-    return "", fmt.Errorf("Unknown error: %w", err)
+    return []string{}, fmt.Errorf("Unknown error: %w", err)
   } 
+  
+  locationData := locationData{}
+  err = json.Unmarshal(body, &locationData)
 
-  return string(body), nil
+  if err != nil {
+    fmt.Print("I ran!!!")
+    return []string{}, err
+  }
+
+  locationList := []string{}
+
+  for i, location := range locationData.Results {
+    locationList = append(locationList, location[i].Name)
+  }
+
+  return locationList , nil
+
+  
 }
